aa8b4e793d84fc59232e5af3cdd2e73e2e4b6262
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-15 18:52:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(integers)
+  array=[]
+  integers.sort do |a, b|
+    a <=> b
+ end   
+end
 
+def sort_array_desc(integers)
+integers.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    1
+  elsif a > b
+    -1
+  end
+end
+end
+
+def sort_array_char_count(integers)
+  integers.sort do |a, b|
+    if a.length == b.length
+     0
+    elsif a.length < b.length
+     -1
+    elsif a.length > b.length
+     1
+  end
+ end
+end 
+
+def swap_elements(integers)
+  integers.swap! do [a, b]
+    integers [1], integers [2] = integers [2], integers [1]
+end
+end 
